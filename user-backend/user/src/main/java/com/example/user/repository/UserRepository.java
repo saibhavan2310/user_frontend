e59cfc381f4b5664c9c93ca33a7ef28219538182
Sai Bhavan Gubbala <54927558+saibhavan2310@users.noreply.github.com>
2025-03-09 12:25:54 +0530

    Delete user-backend directory

--- user-backend/user/src/main/java/com/example/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.user.repository;
-
-import com.example.user.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
